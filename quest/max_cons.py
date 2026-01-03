1377da126e30bd3efd33ca786c8846a75eb96e15
Sirius1616 <siriusa1.615@gmail.com>
2026-01-04 00:13:26 +0100

    reshuffling a list

--- quest/max_cons.py
+++ quest/max_cons.py
@@ -4,5 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in 
+        temp = 0
+        temp_list = []
+        len_num = len(nums)
+        for i in range(len_num):
+            
         
